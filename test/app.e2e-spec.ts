b83a2d9bed544e225241c1a806f05b18298e8c09
RanjanKumar666 <typhonazazel@gmail.com>
2022-05-02 19:57:10 +0530

    tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { writeFile } from 'fs/promises';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
@@ -15,10 +16,37 @@ describe('AppController (e2e)', () => {
         await app.init();
     });
 
-    it('/ (GET)', () => {
+    it('api/generate/docx (POST)', async () => {
         return request(app.getHttpServer())
-            .get('/')
-            .expect(200)
-            .expect('Hello World!');
+            .post('/api/generate/docx/')
+            .set('Content-Type', 'multipart/form-data')
+            .attach(
+                'files',
+                `${__dirname}/test-images/patrick-schrodter-X2c-eOO4Evo-unsplash.jpg`,
+            )
+            .expect(async (res) => {
+                await writeFile(
+                    `${__dirname}/test-images/document.docx`,
+                    res.text,
+                );
+            })
+            .expect(201);
+    });
+
+    it('api/generate/pdf (POST)', async () => {
+        return request(app.getHttpServer())
+            .post('/api/generate/pdf/')
+            .set('Content-Type', 'multipart/form-data')
+            .attach(
+                'files',
+                `${__dirname}/test-images/patrick-schrodter-X2c-eOO4Evo-unsplash.jpg`,
+            )
+            .expect(async (res) => {
+                await writeFile(
+                    `${__dirname}/test-images/document.pdf`,
+                    res.body,
+                );
+            })
+            .expect(201);
     });
 });
